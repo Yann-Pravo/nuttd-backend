051e3eac4af2627e8652289701d89092b2bb365e
Yann Pravo <yann.pravo@gmail.com>
2024-05-31 00:17:16 +0200

    change cookie setting

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ app.use(
     resave: true,
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      secure: true,
       sameSite: 'none',
     },
     store: new PrismaSessionStore(client, {
